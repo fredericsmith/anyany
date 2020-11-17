369966122fb5d5cafb62186d0787c8b1d73ec966
frederic <879832028@qq.com>
2020-11-18 02:03:12 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">
+        <HelloWorld msg="随时随地触碰生活" />
+    </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+    name: 'App',
+    components: {
+        HelloWorld
+    }
+}
+</script>
+
+<style>
+#app {
+}
+</style>
